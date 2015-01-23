3b213f22b54c0a34e7fdc392e3d00bf20acca06d
phucanh203 <nguyenphuc.203@gmail.com>
2015-01-24 00:55:34 +0700

    tao project

---
+++ src/common/GenerateItorator.java
@@ -0,0 +1,7 @@
+package common;
+
+import java.util.Iterator;
+
+public interface GenerateItorator<V> extends Iterator<V> {
+	
+}
---
+++ src/common/LongIterator.java
@@ -0,0 +1,7 @@
+package common;
+
+import java.util.Iterator;
+
+public interface LongIterator extends GenerateIterator<Long> {
+
+}
